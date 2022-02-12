7bcdcf8c82480d39296917dace4a2f8dd231e6d4
shalinic26 <shalinichintala@gmail.com>
2022-02-12 19:51:52 +0530

    First Java code from Eclipse

---
+++ src/Pages/HomePage.java
@@ -0,0 +1,11 @@
+package Pages;
+
+public class HomePage {
+	
+	public static void main(String a[])
+	{
+		System.out.println("Hello HP");
+	}
+	
+
+}
---
+++ src/Tests/LoginPageTest.java
@@ -0,0 +1,11 @@
+package Tests;
+
+public class LoginPageTest {
+	
+	public void test1()
+	{
+		System.out.println("test 1 - LP");
+	}
+	
+
+}
